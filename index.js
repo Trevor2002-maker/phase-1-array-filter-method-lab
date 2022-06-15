1fcba0f2d00b90a7649bb2b2c535f6890ca09e4e
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2022-06-15 09:19:00 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers, string){
+    const names = drivers.filter(function(name){
+        return name.toUpperCase() === string.toUpperCase();
+    });
+return names;
+}
+findMatching(drivers,"Bo")
+
+function fuzzyMatch(drivers){
+   const drivers = [];
+   return undefined;
+}
